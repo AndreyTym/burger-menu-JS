ae0311e468b13ccd56188d7fd1f1ae36a5bf3f61
Andrey <tymoshenko.andrey@gmail.com>
2020-10-18 23:30:01 +0300

    fix: change name of classes

--- js/burger.js
+++ js/burger.js
@@ -1,17 +1,17 @@
-const hamburger = document.querySelector(".header__burger");
-const navList = document.querySelector(".nav__list");
+const burger = document.querySelector(".burger__menu");
+const list = document.querySelector(".list");
 const notOnly = document.querySelector(".not-only");
 const logo = document.querySelector(".logo");
 const notOnlyOverlay = document.querySelector(".not-only__overlay");
-const navLinkActive = document.querySelector(".nav__link_active");
+const listLinkActive = document.querySelector(".list__link_active");
 const container320 = document.querySelector(".container_320");
 
-let elementsArr = [navLinkActive, notOnlyOverlay, hamburger];
+let elementsArr = [listLinkActive, notOnlyOverlay, burger];
 
-elementsArr.forEach((element) => {
-  element.addEventListener("click", () => {
-    hamburger.classList.toggle("header__burger_active");
-    navList.classList.toggle("nav__list_active");
+elementsArr.forEach((e) => {
+  e.addEventListener("click", () => {
+    burger.classList.toggle("burger__menu_active");
+    list.classList.toggle("list_active");
     notOnly.classList.toggle("not-only_active");
     logo.classList.toggle("logo_active");
     notOnlyOverlay.classList.toggle("not-only__overlay_active");

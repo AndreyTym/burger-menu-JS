ee1b94b13f6f22ddd198ad2b6f9acb6664a40f33
Andrey <tymoshenko.andrey@gmail.com>
2020-10-19 11:54:47 +0300

    fix: clear css file

--- js/burger.js
+++ js/burger.js
@@ -1,20 +1,20 @@
 const burger = document.querySelector(".burger__menu");
 const list = document.querySelector(".list");
-const notOnly = document.querySelector(".not-only");
+const hero = document.querySelector(".hero");
 const logo = document.querySelector(".logo");
-const notOnlyOverlay = document.querySelector(".not-only__overlay");
+const heroCover = document.querySelector(".hero__cover");
 const listLinkActive = document.querySelector(".list__link_active");
 const container320 = document.querySelector(".container_320");
 
-let elementsArr = [listLinkActive, notOnlyOverlay, burger];
+let elementsArr = [listLinkActive, heroCover, burger];
 
 elementsArr.forEach((e) => {
   e.addEventListener("click", () => {
     burger.classList.toggle("burger__menu_active");
     list.classList.toggle("list_active");
-    notOnly.classList.toggle("not-only_active");
+    hero.classList.toggle("hero_active");
     logo.classList.toggle("logo_active");
-    notOnlyOverlay.classList.toggle("not-only__overlay_active");
+    heroCover.classList.toggle("hero__cover_active");
     container320.classList.toggle("container_320_active");
   });
 });
